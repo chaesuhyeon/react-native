82030b87b5da3be4f799d70f8e70ffc412a62db3
suhyeon chae <ddd8177@gmail.com>
2023-11-22 20:48:03 +0900

    날씨 api 연동

--- NomadWeather2/App.js
+++ NomadWeather2/App.js
@@ -3,80 +3,87 @@ import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
+const API_KEY = "9c1841791c0e195c6b60fc7d751eb757";
+
 export default function App() {
-    const [city, setCity] = useState("Loading...");
-    const [location, setLocation] = useState();
-    const [ok, setOk] = useState(true);
+  const [city, setCity] = useState("Loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
 
-    const ask = async () => {
-        // 위치 정보 권한 요청 허용 여부
-        const { granted } = await Location.requestForegroundPermissionsAsync();
+  const getWeather = async () => {
+    // 위치 정보 권한 요청 허용 여부
+    const { granted } = await Location.requestForegroundPermissionsAsync();
 
-        if (!granted) {
-            setOk(false);
-        }
+    if (!granted) {
+      setOk(false);
+    }
 
-        const {
-            coords: { latitude, longitude },
-        } = await Location.getCurrentPositionAsync({
-            accuracy: 5,
-        });
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({
+      accuracy: 5,
+    });
 
-        const location = await Location.reverseGeocodeAsync(
-            {
-                latitude,
-                longitude,
-            },
-            { useGoogleMaps: false }
-        );
-        setCity(location[0].city);
-    };
-    useEffect(() => {
-        ask();
-    }, []);
-
-    return (
-        <View style={styles.container}>
-            <View style={styles.city}>
-                <Text style={styles.cityName}>{city}</Text>
-            </View>
-            <View style={styles.weather}>
-                <View style={styles.day}>
-                    <Text style={styles.temp}>27</Text>
-                    <Text style={styles.description}>Sunny</Text>
-                </View>
-            </View>
-        </View>
+    const location = await Location.reverseGeocodeAsync(
+      {
+        latitude,
+        longitude,
+      },
+      { useGoogleMaps: false }
     );
+    setCity(location[0].city);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}`
+    );
+    const json = await response.json();
+    console.log(json);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "skyblue",
-    },
-    city: {
-        flex: 1.2,
-        justifyContent: "center",
-        alignItems: "center",
-    },
-    cityName: {
-        fontSize: 68,
-        fpmtWeight: "500",
-    },
-    weather: {
-        flex: 3,
-    },
-    day: {
-        flex: 1,
-        alignItems: "center",
-    },
-    temp: {
-        marginTop: 50,
-        fontSize: 178,
-    },
-    description: {
-        marginTop: -30,
-        fontSize: 60,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "skyblue",
+  },
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+    flex: 3,
+  },
+  day: {
+    flex: 1,
+    alignItems: "center",
+  },
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60,
+  },
 });
