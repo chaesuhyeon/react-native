94a8032994566912c8aa95fb6debf9bc9654cf00
suhyeon chae <ddd8177@gmail.com>
2023-11-28 23:17:36 +0900

    모달 적용

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,66 +1,87 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
 import {
-    Button,
-    StyleSheet,
-    Text,
-    TextInput,
-    View,
-    ScrollView,
-    FlatList,
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  ScrollView,
+  FlatList,
 } from "react-native";
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
-    const [courseGoals, setCourseGoals] = useState([]);
+  const [courseGoals, setCourseGoals] = useState([]);
+  const [modalIsVisible, setModalIsVisible] = useState(false);
 
-    function addGoalHandler(enteredGoalText) {
-        setCourseGoals((currentCourseGoals) => [
-            ...currentCourseGoals,
-            { text: enteredGoalText, key: Math.random().toString() }, // FlatList에서는 key가 필요한데, 이렇게 key를 지정해주면 알아서 key를 잘 찾는다.
-        ]);
-    }
+  function startAddGoalHandler() {
+    setModalIsVisible(true);
+  }
 
-    function deleteGoalHandler(id) {
-        setCourseGoals((currentCourseGoals) => {
-            return currentCourseGoals.filter((goal) => goal.key !== id);
-        });
-    }
+  function endAddGoalHandler() {
+    setModalIsVisible(false);
+  }
 
-    return (
-        <View style={styles.appContainer}>
-            <GoalInput onAddGoal={addGoalHandler} />
-            <View style={styles.goalsContainer}>
-                <FlatList
-                    data={courseGoals}
-                    renderItem={(itemData) => {
-                        return (
-                            <GoalItem
-                                text={itemData.item.text}
-                                id={itemData.item.key}
-                                onDeleteItem={deleteGoalHandler}
-                            />
-                        );
-                    }}
-                    // 아래 작업은 실제 데이터에 key라는 변수명이 없을 때 해당 key값을 별도로 지정해주는 작업
-                    // keyExtractor={(item, index) => {
-                    //     return item.id;
-                    // }}
-                    alwaysBounceVertical={false}
-                />
-            </View>
-        </View>
-    );
+  function addGoalHandler(enteredGoalText) {
+    setCourseGoals((currentCourseGoals) => [
+      ...currentCourseGoals,
+      { text: enteredGoalText, key: Math.random().toString() }, // FlatList에서는 key가 필요한데, 이렇게 key를 지정해주면 알아서 key를 잘 찾는다.
+    ]);
+
+    endAddGoalHandler();
+  }
+
+  function deleteGoalHandler(id) {
+    setCourseGoals((currentCourseGoals) => {
+      return currentCourseGoals.filter((goal) => goal.key !== id);
+    });
+  }
+
+  return (
+    <View style={styles.appContainer}>
+      <Button
+        title="Add New Goal"
+        color={"#5e0acc"}
+        onPress={startAddGoalHandler}
+      />
+
+      <GoalInput
+        onAddGoal={addGoalHandler}
+        onCancel={endAddGoalHandler}
+        visible={modalIsVisible}
+      />
+      <View style={styles.goalsContainer}>
+        <FlatList
+          data={courseGoals}
+          renderItem={(itemData) => {
+            return (
+              <GoalItem
+                text={itemData.item.text}
+                id={itemData.item.key}
+                onDeleteItem={deleteGoalHandler}
+              />
+            );
+          }}
+          // 아래 작업은 실제 데이터에 key라는 변수명이 없을 때 해당 key값을 별도로 지정해주는 작업
+          // keyExtractor={(item, index) => {
+          //     return item.id;
+          // }}
+          alwaysBounceVertical={false}
+        />
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    appContainer: {
-        flex: 1,
-        paddingTop: 50,
-        paddingHorizontal: 16,
-    },
-    goalsContainer: {
-        flex: 5,
-    },
+  appContainer: {
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
+  },
+  goalsContainer: {
+    flex: 5,
+  },
 });
--- RNCourse/components/GoalInput.js
+++ RNCourse/components/GoalInput.js
@@ -1,47 +1,63 @@
 import { useState } from "react";
-import { StyleSheet, View, TextInput, Button } from "react-native";
+import { StyleSheet, View, TextInput, Button, Modal } from "react-native";
 function GoalInput(props) {
-    const [enteredGoalText, setEnteredGoalText] = useState("");
-    function goalInputHandler(enterdText) {
-        setEnteredGoalText(enterdText);
-    }
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  function goalInputHandler(enterdText) {
+    setEnteredGoalText(enterdText);
+  }
 
-    function addGoalHandler() {
-        props.onAddGoal(enteredGoalText);
-        setEnteredGoalText("");
-    }
+  function addGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
 
-    return (
-        <View style={styles.inputContainer}>
-            <TextInput
-                style={styles.textInput}
-                placeholder="Your course goal!"
-                onChangeText={goalInputHandler}
-                value={enteredGoalText}
-            />
+  return (
+    <Modal visible={props.visible} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your course goal!"
+          onChangeText={goalInputHandler}
+          value={enteredGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
             <Button title="Add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Cancel" onPress={props.onCancel} />
+          </View>
         </View>
-    );
+      </View>
+    </Modal>
+  );
 }
 
 export default GoalInput;
 
 const styles = StyleSheet.create({
-    inputContainer: {
-        flex: 1,
-        flexDirection: "row",
-        alignItems: "center",
-        justifyContent: "space-between",
-        marginBottom: 24,
-        borderBottomWidth: 1,
-        borderBottomColor: "#ccc",
-    },
-    textInput: {
-        borderColor: "#ccc",
-        borderWidth: 1,
-        padding: 1,
-        width: "70%",
-        marginRight: 8,
-        padding: 8,
-    },
+  inputContainer: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 24,
+    padding: 16,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+  },
+  textInput: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 1,
+    width: "100%",
+    padding: 8,
+  },
+  buttonContainer: {
+    marginTop: 16,
+    flexDirection: "row",
+  },
+  button: {
+    width: 100,
+    marginHorizontal: 8,
+  },
 });
